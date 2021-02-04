72a4ddebb38bbc752d19b07409968840c04537ed
Michael Frajman <mhf420@gmail.com>
2021-02-03 22:50:23 -0500

    attempt at cache tcl file

--- Group08_Cache/cache_tb.tcl
+++ Group08_Cache/cache_tb.tcl
@@ -1,19 +1,31 @@
 proc AddWaves {} {
 	;#Add waves we're interested in to the Wave window
-    add wave -position end sim:/fsm_tb/clk
-    add wave -position end sim:/fsm_tb/s_reset
-    add wave -position end sim:/fsm_tb/s_input
-    add wave -position end sim:/fsm_tb/s_output
+    add wave -position end sim:/cache_tb/clock
+    add wave -position end sim:/cache_tb/reset
+
+    add wave -position end sim:/cache_tb/s_addr
+    add wave -position end sim:/cache_tb/s_read
+    add wave -position end sim:/cache_tb/s_readdata
+    add wave -position end sim:/cache_tb/s_write
+    add wave -position end sim:/cache_tb/s_writedata
+    add wave -position end sim:/cache_tb/s_waitrequest
+
+    add wave -position end sim:/cache_tb/m_addr
+    add wave -position end sim:/cache_tb/m_read
+    add wave -position end sim:/cache_tb/m_readdata
+    add wave -position end sim:/cache_tb/m_write
+    add wave -position end sim:/cache_tb/m_writedata
+    add wave -position end sim:/cache_tb/m_waitrequest
 }
 
 vlib work
 
 ;# Compile components if any
-vcom fsm.vhd
-vcom fsm_tb.vhd
+vcom cache.vhd
+vcom cache_tb.vhd
 
 ;# Start simulation
-vsim fsm_tb
+vsim cache_tb
 
 ;# Generate a clock with 1ns period
 force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
